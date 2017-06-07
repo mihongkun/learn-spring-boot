15888b66c2e06b87fd6de2021831badab89fcd71
mihongkun <mihongkun@gmail.com>
2017-06-07 18:34:18 +0800

    创建hello world!

--- src/main/java/com/mi/learnspringboot/com/mi/learnspringboot/controller/HelloController.java
+++ src/main/java/com/mi/learnspringboot/com/mi/learnspringboot/controller/HelloController.java
@@ -1,5 +1,14 @@
 package com.mi.learnspringboot.com.mi.learnspringboot.controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class HelloController {
+
+    @GetMapping("/hello")
+    public String hello(){
+        return "Hello World!";
+    }
 
 }
